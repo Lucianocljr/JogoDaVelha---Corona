ad0273542ce04aa32cebd099caed0a344fb2c260
LUCIANO CAMPOS DE LIMA JUNIOR <20161D13GR0210@LABS.GARANHUNS.IFPE>
2018-06-07 20:34:45 -0300

    Criacao das jogadas do bola

--- main.lua
+++ main.lua
@@ -41,35 +41,154 @@ botao9:setFillColor( 0 )
 --jogadaX1 = display.newLine(0, display.actualContentHeight/3, display.actualContentWidth/3,  0,
 						   --0, 0,display.actualContentWidth/3, display.actualContentWidth/2)
 
---jogadaX2 = display.newLine(display.actualContentWidth/3, 0, display.actualContentWidth/1.5,  display.actualContentHeight/3, 
+--jogadaX2 = display.newLine(display.actualContentWidth/3, 0, display.actualContentWidth/1.5,  display.actualContentHeight/3,
 						   --display.actualContentWidth/1.5, 0, display.actualContentWidth/3, display.actualContentHeight/3)
 
---jogadaX3 = display.newLine(display.actualContentWidth, 0, display.actualContentWidth/1.5,  display.actualContentHeight/3, 
+--jogadaX3 = display.newLine(display.actualContentWidth, 0, display.actualContentWidth/1.5,  display.actualContentHeight/3,
 						   --display.actualContentWidth/1.5, 0, display.actualContentWidth, display.actualContentHeight/3)
+
+
+--jogadaX4 = display.newLine(display.actualContentWidth/3, display.actualContentHeight/3, 0,  display.actualContentHeight/1.5,
+						   --0, display.actualContentHeight/3,display.actualContentWidth/3, display.actualContentWidth)
+
+--jogadaX5 = display.newLine(display.actualContentWidth/1.5, display.actualContentHeight/3, display.actualContentWidth/3,  display.actualContentHeight/1.5,
+						   --display.actualContentWidth/3, display.actualContentHeight/3,display.actualContentWidth/1.5, display.actualContentWidth)
+
+--jogadaX6 = display.newLine(display.actualContentWidth, display.actualContentHeight/3, display.actualContentWidth/1.5,  display.actualContentHeight/1.5,
+						   --display.actualContentWidth/1.5, display.actualContentHeight/3,display.actualContentWidth, display.actualContentWidth)
+
+--jogadaX7 = display.newLine(display.actualContentWidth/3, display.actualContentHeight/1.5, 0,  display.actualContentHeight,
+						   --0, display.actualContentHeight/1.5,display.actualContentWidth/3, display.actualContentHeight)
+
+--jogadaX8 = display.newLine(display.actualContentWidth/1.5, display.actualContentHeight/1.5, display.actualContentWidth/3,  display.actualContentHeight,
+						   --display.actualContentWidth/3, display.actualContentHeight/1.5,display.actualContentWidth/1.5, display.actualContentHeight)
+
+--jogadaX9 = display.newLine(display.actualContentWidth, display.actualContentHeight/1.5, display.actualContentWidth/1.5,  display.actualContentHeight,
+						   --display.actualContentWidth/1.5, display.actualContentHeight/1.5,display.actualContentWidth, display.actualContentHeight)  
 
 --Posição das jogadas com X
 
+--Posição das jogadas com O
+
+--jogadaO1 = display.newCircle(display.actualContentWidth/6,display.actualContentHeight/6, 48)
+--jogadaO1:setFillColor( 0.0 )
+--jogadaO1.strokeWidth = 2
+--jogadaO1:setStrokeColor( 1, 1, 1 )
+
+--jogadaO2 = display.newCircle(display.actualContentWidth/2,display.actualContentHeight/6, 48)
+--jogadaO2:setFillColor( 0.0 )
+--jogadaO2.strokeWidth = 2
+--jogadaO2:setStrokeColor( 1, 1, 1 )
+
+--jogadaO3 = display.newCircle(display.actualContentWidth/1.2,display.actualContentHeight/6, 48)
+--jogadaO3:setFillColor( 0.0 )
+--jogadaO3.strokeWidth = 2
+--jogadaO3:setStrokeColor( 1, 1, 1 )
+
+--jogadaO4 = display.newCircle(display.actualContentWidth/6,display.actualContentHeight/2, 48)
+--jogadaO4:setFillColor( 0.0 )
+--jogadaO4.strokeWidth = 2
+--jogadaO4:setStrokeColor( 1, 1, 1 )
+
+--jogadaO5 = display.newCircle(display.actualContentWidth/2,display.actualContentHeight/2, 48)
+--jogadaO5:setFillColor( 0.0 )
+--jogadaO5.strokeWidth = 2
+--jogadaO5:setStrokeColor( 1, 1, 1 )
+
+--jogadaO6 = display.newCircle(display.actualContentWidth/1.2,display.actualContentHeight/2, 48)
+--jogadaO6:setFillColor( 0.0 )
+--jogadaO6.strokeWidth = 2
+--jogadaO6:setStrokeColor( 1, 1, 1 )
+
+--jogadaO7 = display.newCircle(display.actualContentWidth/6,display.actualContentHeight/1.2, 48)
+--jogadaO7:setFillColor( 0.0 )
+--jogadaO7.strokeWidth = 2
+--jogadaO7:setStrokeColor( 1, 1, 1 )
+
+--jogadaO8 = display.newCircle(display.actualContentWidth/2,display.actualContentHeight/1.2, 48)
+--jogadaO8:setFillColor( 0.0 )
+--jogadaO8.strokeWidth = 2
+--jogadaO8:setStrokeColor( 1, 1, 1 )
+
+--jogadaO9 = display.newCircle(display.actualContentWidth/1.2,display.actualContentHeight/1.2, 48)
+--jogadaO9:setFillColor( 0.0 )
+--jogadaO9.strokeWidth = 2
+--jogadaO9:setStrokeColor( 1, 1, 1 )
+
+
+--Posição das jogadas com O
+
 --testes
 
-local options = 
+--tabuleiro
+tabuleiro = {{' ',' ',' '},
+            {' ',' ',' '},
+            {' ',' ',' '}}
+
+jogador = 'O'
+--tabulerio
+
+local options =
 {
-    text = "Luciano Júnior",     
+    text = "Luciano Júnior",
     x = 100,
     y = 200,
     width = 128,
-    font = native.systemFont,   
+    font = native.systemFont,
     fontSize = 18,
     align = "right"  -- Alignment parameter
 }
 
-local function jogada (event)
+local function jogadaDoBotao1 (event)
 
-	display.newText(options)
+    if event.phase == "began" then
 
-end	
+        if jogador == 'O' then 
+        tabuleiro[1][1] = 'O'
+        jogadaO1 = display.newCircle(display.actualContentWidth/6,display.actualContentHeight/6, 48)
+        jogadaO1:setFillColor( 0.0 )
+        jogadaO1.strokeWidth = 2
+        jogadaO1:setStrokeColor( 1, 1, 1 )
+        jogador = 'X'
 
-botao1:addEventListener("touch", jogada)
+        else
 
+        tabuleiro[1][1] = 'X'
+        jogadaX1 = display.newLine(0, display.actualContentHeight/3, display.actualContentWidth/3,  0,
+						   0, 0,display.actualContentWidth/3, display.actualContentWidth/2)
+        jogador = 'O'
+
+        end
+        
+    end
+end
+local function jogadaDoBotao2 (event)
+
+    if event.phase == "began" then
+
+        if jogador == 'O' then 
+        tabuleiro[1][1] = 'O'
+        jogadaO2 = display.newCircle(display.actualContentWidth/2,display.actualContentHeight/6, 48)
+        jogadaO2:setFillColor( 0.0 )
+        jogadaO2.strokeWidth = 2
+        jogadaO2:setStrokeColor( 1, 1, 1 )
+        jogador = 'X'
+
+        else
+
+        tabuleiro[1][1] = 'X'
+        jogadaX2 = display.newLine(display.actualContentWidth/3, 0, display.actualContentWidth/1.5,  display.actualContentHeight/3,
+						   display.actualContentWidth/1.5, 0, display.actualContentWidth/3, display.actualContentHeight/3)
+        jogador = 'O'
+
+        end
+
+    end
+end
+
+botao1:addEventListener("touch", jogadaDoBotao1)
+botao2:addEventListener("touch", jogadaDoBotao2)
 
 
 --testes
+
