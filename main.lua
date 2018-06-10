e780d929b9b1796f88495d03361d5110c9be8180
Lucianocljr <lucianocljr@hotmail.com>
2018-06-10 13:53:15 -0300

    Ajustes no tabuleiro

--- main.lua
+++ main.lua
@@ -1,9 +1,13 @@
-
 --Tabuleiro
+
+function gerarTabuleiro( )
+
 local linhaHorizontal1 = display.newLine( 0,  display.actualContentHeight/3, display.actualContentWidth, display.actualContentHeight/3 )
 local linhaHorizontal2 = display.newLine( 0,  display.actualContentHeight/1.5, display.actualContentWidth, display.actualContentHeight/1.5 )
 local linhaVertical1 = display.newLine( display.actualContentWidth/3,  0, display.actualContentWidth/3, display.actualContentHeight )
 local linhaVertical2 = display.newLine( display.actualContentWidth/1.5,  0, display.actualContentWidth/1.5, display.actualContentHeight )
+	return true
+end
 --Tabuleiro
 
 --Botões
